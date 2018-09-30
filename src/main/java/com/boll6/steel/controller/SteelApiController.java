3e7a435a2d28e8eb9be98604c445feaf09b363ba
LinZain <linzp_01@outlook.com>
2018-09-30 11:04:34 +0800

    linzp

--- src/main/java/com/boll6/steel/controller/SteelApiController.java
+++ src/main/java/com/boll6/steel/controller/SteelApiController.java
@@ -11,6 +11,10 @@ import com.boll6.steel.service.IUserService;
 import com.boll6.steel.util.NumberUtils;
 import com.boll6.steel.vo.ICSVO;
 import com.boll6.steel.vo.SteelNumberVO;
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.FileUploadException;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
 import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -20,7 +24,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
+import java.io.*;
 import java.math.BigInteger;
 import java.util.List;
 
@@ -257,6 +261,76 @@ public class SteelApiController {
             }
             jsonObject.put("rows", infomationList);
             return jsonObject.toString();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+
+    @ResponseBody
+    @RequestMapping(value = {"/uploadPic"}, method = {RequestMethod.GET})
+    public String uploadPic(HttpServletRequest request, HttpServletResponse response) {
+        JSONObject jsonObject = new JSONObject();
+        String path = request.getRealPath("/upload") + "/";
+        File dir = new File(path);
+        if (!dir.exists()) {
+            dir.mkdir();
+        }
+        //获得磁盘文件条目工厂
+        DiskFileItemFactory factory = new DiskFileItemFactory();
+
+        //如果没以下两行设置的话,上传大的文件会占用很多内存，
+        //设置暂时存放的存储室,这个存储室可以和最终存储文件的目录不同
+        /**
+         * 原理: 它是先存到暂时存储室，然后再真正写到对应目录的硬盘上，
+         * 按理来说当上传一个文件时，其实是上传了两份，第一个是以 .tem 格式的
+         * 然后再将其真正写到对应目录的硬盘上
+         */
+        factory.setRepository(dir);
+        //设置缓存的大小，当上传文件的容量超过该缓存时，直接放到暂时存储室
+        factory.setSizeThreshold(1024 * 1024);
+        //高水平的API文件上传处理
+        ServletFileUpload upload = new ServletFileUpload(factory);
+        try {
+            List<FileItem> list = upload.parseRequest(request);
+            FileItem picture = null;
+            for (FileItem item : list) {
+                //获取表单的属性名字
+                String name = item.getFieldName();
+                //如果获取的表单信息是普通的 文本 信息
+                if (item.isFormField()) {
+                    //获取用户具体输入的字符串
+                    String value = item.getString();
+                    request.setAttribute(name, value);
+                } else {
+                    picture = item;
+                }
+            }
+
+            //自定义上传图片的名字为userId.jpg
+            String fileName = request.getAttribute("userId") + ".jpg";
+            String destPath = path + fileName;
+
+            //真正写到磁盘上
+            File file = new File(destPath);
+            OutputStream out = new FileOutputStream(file);
+            InputStream in = picture.getInputStream();
+            int length = 0;
+            byte[] buf = new byte[1024];
+            // in.read(buf) 每次读到的数据存放在buf 数组中
+            while ((length = in.read(buf)) != -1) {
+                //在buf数组中取出数据写到（输出流）磁盘上
+                out.write(buf, 0, length);
+            }
+            in.close();
+            out.close();
+            jsonObject.put("url", destPath);
+            jsonObject.put("success", true);
+            return jsonObject.toJSONString();
+        } catch (FileUploadException e1) {
+            e1.printStackTrace();
+            return null;
         } catch (Exception e) {
             e.printStackTrace();
             return null;

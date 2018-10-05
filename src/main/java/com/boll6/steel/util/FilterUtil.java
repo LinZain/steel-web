a14f22fc55b0c0087005cc3be1d497a9aae1d6ed
Dv_mo <Dv_mo@DESKTOP-5777B7R>
2018-10-05 21:23:06 +0800

    change some filtters

--- src/main/java/com/boll6/steel/util/FilterUtil.java
+++ src/main/java/com/boll6/steel/util/FilterUtil.java
@@ -40,11 +40,11 @@ public class FilterUtil implements Filter {
 
     protected static boolean sqlValidate(String str) {
         str = str.toLowerCase();
-        String badStr = "'|and|exec|execute|insert|select|delete|update|count|drop|*|%|chr|mid|master|truncate|"
-                + "char|declare|sitename|net user|xp_cmdshell|;|or|+|,|like'|and|exec|execute|insert|create|drop|"
+        String badStr = "'|exec|execute|insert|select|delete|update|count|drop|master|truncate|"
+                + "declare|sitename|net user|xp_cmdshell|like'|exec|execute|insert|create|drop|"
                 + "table|from|grant|use|group_concat|column_name|"
-                + "information_schema.columns|table_schema|union|where|select|delete|update|order|by|count|*|"
-                + "chr|mid|master|truncate|char|declare|or|;|--|+|,|like|//|/|%|#";
+                + "information_schema.columns|table_schema|union|where|select|delete|update|order|count|"
+                + "master|truncate|declare|";
         String[] badStrs = badStr.split("\\|");
         for (int i = 0; i < badStrs.length; i++) {
             if (str.indexOf(badStrs[i]) >= 0) {
